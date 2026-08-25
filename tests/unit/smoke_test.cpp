875061f3df699c671b66b1b74681147763ec6a6b
Simon Wen <simonwensw7@gmail.com>
2026-08-24 23:58:08 -0400

    test: add GoogleTest infrastructure with vcpkg

---
+++ tests/unit/smoke_test.cpp
@@ -0,0 +1,12 @@
+#include <array>
+#include <span>
+
+#include <gtest/gtest.h>
+
+TEST(BuildConfigurationTest, SupportsCpp20) {
+    const std::array<int, 3> values{1, 2, 3};
+    const std::span<const int> view{values};
+
+    EXPECT_EQ(view.size(), 3U);
+    EXPECT_EQ(view.front(), 1);
+}
